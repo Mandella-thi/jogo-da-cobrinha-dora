e4130823ab4d4b761b4fd5d3206335da23467260
mandella-thi <thiagogarc@gmail.com>
2020-11-15 16:49:02 -0300

    game over

--- themain.js
+++ themain.js
@@ -35,11 +35,18 @@ if(event.keyCode==39 && direction!= "left") direction="right";
 if (event.keyCode==40 && direction!= "up") direction="down";
     }
 function iniciarjogo(){
+    
     if(snake[0].x>15 * box &&direction == "right") snake[0].x =0;
 if(snake[0].x<0 && direction== "left") snake[0].x =16 * box;
 if(snake[0].y>15 * box && direction== "down")snake[0].y=0;
 if(snake[0].y <0 && direction =="up") snake[0].y= 16 * box;
-    criarBG();
+for(i=1; i<snake.length;i++) {
+    if(snake[0].x ==snake[i].x && snake[0].y == snake[i].y){
+        clearInterval(jogo);
+        alert("game over :(");
+    }
+}   
+criarBG();
     criarCobrinha();
     drawFood()
     let snakeX=snake[0].x;
